ee134344d1fa3040dce41484cd1a1bb3a11878b8
Rohan Modi <rohan@rohanmodi.ca>
2024-07-19 16:43:03 -0700

    fixes

--- lua/start.sh
+++ lua/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+fuser -k 63001/tcp
+fuser -k 63002/tcp
 /usr/bin/env python3 $HOME'/.local/share/nvim/plugged/KaVimTex/backend/viewer/webkit_viewer.py' &
 node $HOME'/.local/share/nvim/plugged/KaVimTex/backend/process.js' &
