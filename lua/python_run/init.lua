070020fc69bfba45dbbc6204c9f5f4a8968b2087
Rohan Modi <rohan@rohanmodi.ca>
2024-07-18 22:27:48 -0700

    fixes

--- lua/python_run/init.lua
+++ lua/python_run/init.lua
@@ -3,7 +3,7 @@ local lfs = require('lfs')
 local pwd = lfs.currentdir()
 pwd = pwd:sub(1,-16) -- there has got to be a better way than this.
 
-local webkit_location = pwd + "backend/viewer/webkit_viewer.py"
+local webkit_location = pwd .. "backend/viewer/webkit_viewer.py"
 
-os.execute("/usr/bin/env python3 " + webkit_location)
+os.execute("/usr/bin/env python3 " .. webkit_location)
 
