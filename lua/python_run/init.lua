7a2aa64791af2b5162bbe33835977141634bbeb9
Rohan Modi <rohan@rohanmodi.ca>
2024-07-19 01:48:35 -0700

    fixes

--- lua/python_run/init.lua
+++ lua/python_run/init.lua
@@ -1,16 +1,3 @@
-local lfs = require('lfs')
+local webkit_location = vim.g.KVTRoot .. "backend/viewer/webkit_viewer.py"
+os.execute("/usr/bin/env python3 " .. webkit_location)
 
-local pwd = lfs.currentdir()
-vim.cmd [[
-	let s:plugindir = expand('<sfile>:p:h:h')
-
-]]
-
-pwd = pwd:sub(1,-15) -- there has got to be a better way than this.
-pwd = s:plugindir
-
--- local webkit_location = pwd .. "backend/viewer/webkit_viewer.py"
-
---print("/usr/bin/env python3 " .. webkit_location)
-
-print(pwd)
