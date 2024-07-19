802917c3818d7299d4fbd87dd3f0e97bfd26e4ce
Rohan Modi <rohan@rohanmodi.ca>
2024-07-19 01:23:31 -0700

    fixes

--- lua/python_run/init.lua
+++ lua/python_run/init.lua
@@ -9,7 +9,8 @@ vim.cmd [[
 pwd = pwd:sub(1,-15) -- there has got to be a better way than this.
 pwd = s:plugindir
 
-local webkit_location = pwd .. "backend/viewer/webkit_viewer.py"
+-- local webkit_location = pwd .. "backend/viewer/webkit_viewer.py"
 
-print("/usr/bin/env python3 " .. webkit_location)
+--print("/usr/bin/env python3 " .. webkit_location)
 
+print(pwd)
