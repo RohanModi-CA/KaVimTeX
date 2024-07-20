73752067e7b7ca603807738970e177f43103ab91
Rohan Modi <rohan@rohanmodi.ca>
2024-07-20 04:27:34 -0700

    fixes

--- lua/vim_server/init.lua
+++ lua/vim_server/init.lua
@@ -1,2 +1,20 @@
 local start_server = require('vim_server.start_server')
+
+
+local job_id = vim.fn.jobstart('/home/rohan/.local/share/nvim/plugged/KaVimTex/lua/start.sh', {
+  detach = true,  -- Run in the background
+  on_stdout = function(_, data)
+    -- Handle stdout if needed, otherwise leave empty
+  end,
+  on_stderr = function(_, data)
+    -- Handle stderr if needed, otherwise leave empty
+  end
+})
+
+-- You can use job_id to interact with the running job
+
+
+
+
 return start_server
+
