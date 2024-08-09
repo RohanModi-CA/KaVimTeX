5b03d94a2ecc11b1ecb5acefc5771bc117082762
Rohan Modi <rohan@rohanmodi.ca>
2024-08-09 19:38:30 -0400

    fixes

--- backend/process.js
+++ backend/process.js
@@ -68,6 +68,10 @@ const server = net.createServer((socket) => {
 
 
 	socket.on('end', () => {
+
+		exec('notify-send 100', (error, stdout, stderr) => {
+		  console.log(`stdout: ${stdout}`);
+		});
     	console.log('Neovim disconnected.');
 		render.terminateViewer(WEBKIT_PORT);
 
