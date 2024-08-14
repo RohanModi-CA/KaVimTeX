946f59cf4e527024e04b3240adae069d39353aaf
Rohan Modi <rohan@rohanmodi.ca>
2024-08-14 15:17:55 -0400

    fixes

--- backend/viewer/webkit_viewer.py
+++ backend/viewer/webkit_viewer.py
@@ -72,20 +72,10 @@ class MainWindow(QMainWindow):
 
             html = add_css.addCSS(html)
             self.browser.setHtml(html)
-            #self.notify(html)
 
         if html == "KAVIMTEX CONNECTED":
             # self.browser.setHtml(r"""<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Display KVT</title><style>body { display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; } .serif { font-family: "Times New Roman", Times, serif; }</style></head><body><div class="serif">KVT</div></body></html>""")
             self.browser.setHtml("KVT")
-
-    def resize_to_content_height(self, height):
-        # self.resize(self.width(), height)
-        # self.update_html(f"katex {height}")
-        browser_h = self.browser.height()
-        self.browser.page().setZoomFactor(((browser_h ) / height) )
-        
-        self.notify(f"{browser_h} and {height} \n")
-        # self.browser.page().runJavaScript("window.scrollTo(0, document.body.scrollHeight / 2)")
  
     def check_ratio(self):
         def after_height_retrieved(height):
