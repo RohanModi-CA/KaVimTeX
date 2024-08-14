1c1961fb4593c7d469c9fb495c1c57dea694195a
Rohan Modi <rohan@rohanmodi.ca>
2024-08-14 00:01:10 -0400

    fixes

--- backend/viewer/webkit_viewer.py
+++ backend/viewer/webkit_viewer.py
@@ -116,7 +116,7 @@ class MainWindow(QMainWindow):
         trials = 0
         while not self.check_ratio():
 
-            notify(f" {trials}   ")
+            notify(f" buggs{trials}   ")
                 #buggs.write(f"{self.browser.height()} and ratio {ratio}")
 
             ratio = self.page_height / self.browser.height()
