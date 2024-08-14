b5f1f713fda791cb9184e8df4e9d4e9fcba392b0
Rohan Modi <rohan@rohanmodi.ca>
2024-08-14 00:14:03 -0400

    fixes

--- backend/viewer/webkit_viewer.py
+++ backend/viewer/webkit_viewer.py
@@ -123,11 +123,11 @@ class MainWindow(QMainWindow):
             ratio = self.page_height / self.browser.height()
             if ratio > self.ratio_upper_bound:
                 pass
-                current_zoom_factor = self.browser.getZoomFactor()
+                #current_zoom_factor = self.browser.getZoomFactor()
                 #self.browser.setZoomFactor(current_zoom_factor * 0.9)
             elif ratio < self.ratio_lower_bound:
                 pass
-                current_zoom_factor = self.browser.getZoomFactor()
+                #current_zoom_factor = self.browser.getZoomFactor()
                 #self.browser.setZoomFactor(current_zoom_factor * 1.1)
  
                 # self.browser.setZoomFactor(0.7)
