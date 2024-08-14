c63d058869d0eabd3f86300254400df970cf2df6
Rohan Modi <rohan@rohanmodi.ca>
2024-08-13 22:46:15 -0400

    fixes

--- backend/viewer/webkit_viewer.py
+++ backend/viewer/webkit_viewer.py
@@ -105,9 +105,9 @@ class MainWindow(QMainWindow):
     def check_ratio(self):
         self.browser.page().runJavaScript("document.body.scrollWidth;", self.record_page_height)
 
-        # ratio = self.browser.height() / self.page_height
-        #with open("/home/rohan/Documents/FileFolder/minefield/minefield.buggs", "a") as buggs:
-            #buggs.write(f"{self.page_height}")
+        ratio = self.browser.height() / self.page_height
+        with open("/home/rohan/Documents/FileFolder/minefield/minefield.buggs", "a") as buggs:
+            buggs.write(f"{ratio}")
         if ( (self.ratio_lower_bound <= ratio) and (ratio <= self.ratio_upper_bound) ):
             return True
         return False
