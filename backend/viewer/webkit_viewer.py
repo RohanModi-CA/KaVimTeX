29cb7e9a040f02dc12bcfabf85f1634d19fbabd8
Rohan Modi <rohan@rohanmodi.ca>
2024-08-14 01:37:55 -0400

    fixes

--- backend/viewer/webkit_viewer.py
+++ backend/viewer/webkit_viewer.py
@@ -114,24 +114,9 @@ class MainWindow(QMainWindow):
                 return True
             else:
                 self.notify(" done false ")
-                return False
+                #return False
 
         self.browser.page().runJavaScript("document.body.scrollHeight;", after_height_retrieved)
-
-
-
-
-#     def check_ratio(self):
-#         self.browser.page().runJavaScript("document.body.scrollHeight;", lambda height: self.record_page_height(height))
-# 
-#         ratio = self.page_height / self.browser.height()
-#         #self.notify(f"{ratio} and viewer {self.browser.height()}")
-#         self.notify(self.browser.height())
-#         if ( (self.ratio_lower_bound <= ratio) and (ratio <= self.ratio_upper_bound) ):
-#             #self.notify(f"ITS DONE {self.page_height} and viewer {self.browser.height()} TABARNAK")
-#             self.notify("done true")
-#             return True
-#         return False
 
     def text_find_ideal_zoom(self):
         ratio = 0
