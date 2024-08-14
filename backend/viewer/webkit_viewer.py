6d8a0495320e0ecacf3e1c6b4f7c5d4521e97dcc
Rohan Modi <rohan@rohanmodi.ca>
2024-08-14 15:36:47 -0400

    fixes

--- backend/viewer/webkit_viewer.py
+++ backend/viewer/webkit_viewer.py
@@ -96,7 +96,7 @@ class MainWindow(QMainWindow):
                     # self.notify(f"gb {ratio} ")
                     self.browser.page().setZoomFactor(current_zoom_factor * 1.1)
                 
-                self.check_ratio(max_runs=8)
+                self.check_ratio()
 
                 return False
         if (max_runs > 0):
