ad1d98ea8dc532354e579b108b4a67b0c9e5f2b7
Rohan Modi <rohan@rohanmodi.ca>
2024-09-06 18:26:37 -0400

    fixes

--- backend/viewer/webkit_viewer.py
+++ backend/viewer/webkit_viewer.py
@@ -75,9 +75,9 @@ class MainWindow(QMainWindow):
 
         def width_check(width):
             w_ratio = width / self.browser.width()
-            self.notify(self.width_checking_bool and not (self.recursion_count > 110))
             if w_ratio > self.ratio.upper_bound:
                 self.browser.page().setZoomFactor(self.browser.page().zoomFactor() * 0.9)
+                self.notify(self.width_checking_bool and not (self.recursion_count > 110))
                 return self.check_ratio() # recursion
             else:
                 return True
