8b47f109ebbb225da36dc587cc482516d1684fa1
Rohan Modi <rohan@rohanmodi.ca>
2024-09-07 13:08:47 -0400

    fixes

--- backend/process.js
+++ backend/process.js
@@ -56,9 +56,9 @@ const server = net.createServer(async (socket) => {
 			
 			line_number = 5; 
 			
-			notify(line_number)
 			g_l_b_array = render.grabLineNumber(processed_line);
 			
+			notify(g_l_b_array);
 			
 			line_number = g_l_b_array[0];
 			processed_line = g_l_b_array[1];
