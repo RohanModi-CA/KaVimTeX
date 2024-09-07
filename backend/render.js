c5ee298970af1107e75325fa47b079d66f7c42ea
Rohan Modi <rohan@rohanmodi.ca>
2024-09-07 12:00:43 -0400

    fixes

--- backend/render.js
+++ backend/render.js
@@ -54,7 +54,6 @@ function stripMathMode(rawTek) {
     }
 
 	if (cleaned.length >= "\\begin{equation}".length) {
-		cleaned = "katex hello"
 		if (cleaned.substring(0,16) === "\\begin{equation}"){
 			cleaned = cleaned.substring(16);
 		}
@@ -64,6 +63,7 @@ function stripMathMode(rawTek) {
 
 	}
     
+	cleaned = cleaned.length
     return cleaned;
 }
 
