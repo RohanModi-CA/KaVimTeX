a31563fb14d11150779b9ade7e04b8a4c171877e
Rohan Modi <rohan@rohanmodi.ca>
2024-09-06 21:37:02 -0400

    fixes

--- backend/viewer/webkit_viewer.py
+++ backend/viewer/webkit_viewer.py
@@ -75,7 +75,7 @@ class MainWindow(QMainWindow):
 
         def width_check(width):
             self.recursion_count += 1
-            self.notify(width)
+            self.notify(f"{str(width)[:6]} vs {self.browser.width()}")
             
             """if not (width):
                 self.browser.page().setZoomFactor(self.browser.page().zoomFactor() * 0.9)
