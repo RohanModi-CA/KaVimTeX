e38ac6976814d3b6713fc1403e7c0f1411246f02
Rohan Modi <rohan@rohanmodi.ca>
2024-09-10 10:57:30 -0400

    fixes

--- backend/process.js
+++ backend/process.js
@@ -105,17 +105,16 @@ const server = net.createServer(async (socket) => {
 				pdf_path = pdf_path.slice(0, -3) + "pdf";
 
 				try{
-				let { stdout: i3_zathuraCommOut } = await execAsync(`bash -c "comm -12 <(xdotool search --name  'org.pwmt.zathura'  | sort) <(xdotool search --name 'org.pwmt.zathura'  | sort)"`); 
-				let i3_ZathuraCommOutArray = i3_ZathuraCommOut.split("\n");
-				for (pid of i3_ZathuraCommOutArray) {
-					if (pid && pid.trim()) {
-						try {
-							await execAsync(`bash -c "xdotool windowkill ${pid}"`);}
-						catch (error) {}
+					let { stdout: i3_zathuraCommOut } = await execAsync(`bash -c "comm -12 <(xdotool search --name  'org.pwmt.zathura'  | sort) <(xdotool search --name 'org.pwmt.zathura'  | sort)"`); 
+					let i3_ZathuraCommOutArray = i3_ZathuraCommOut.split("\n");
+					for (pid of i3_ZathuraCommOutArray) {
+						if (pid && pid.trim()) {
+							try {
+								await execAsync(`bash -c "xdotool windowkill ${pid}"`);}
+							catch (error) {}
+						}
 					}
-				}
-				}
-				catch (error) {}
+				}		catch (error) {notify(error)}
 				
 
 
