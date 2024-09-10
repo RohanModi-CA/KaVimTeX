a6bf39ef6439918df832b75da87242e673dc2441
Rohan Modi <rohan@rohanmodi.ca>
2024-09-10 11:30:35 -0400

    fixes

--- backend/process.js
+++ backend/process.js
@@ -107,7 +107,7 @@ const server = net.createServer(async (socket) => {
 				pdf_path = pdf_path.slice(0, -3) + "pdf";
 
 				try{
-					let { stdout: i3_ZathuraCommOut } = await execAsync(`bash -c 'xwininfo -tree -root | grep "\"org.pwmt.zathura\": ()" | awk "{print $1}"'`); 
+					let { stdout: i3_ZathuraCommOut } = await execAsync(`bash -c "xwininfo -tree -root | grep '\\\"org.pwmt.zathura\\\": ()' | awk '{print \$1}'"`); 
 
 					let i3_ZathuraCommOutArray = i3_ZathuraCommOut.split("\n");
 					for (pid of i3_ZathuraCommOutArray) {
