c1a03b977e5d8283b99ce1680df1bc83670b415a
Rohan Modi <rohan@rohanmodi.ca>
2024-09-10 10:58:19 -0400

    fixes

--- backend/process.js
+++ backend/process.js
@@ -105,7 +105,7 @@ const server = net.createServer(async (socket) => {
 				pdf_path = pdf_path.slice(0, -3) + "pdf";
 
 				try{
-					let { stdout: i3_zathuraCommOut } = await execAsync(`bash -c "comm -12 <(xdotool search --name  'org.pwmt.zathura'  | sort) <(xdotool search --name 'org.pwmt.zathura'  | sort)"`); 
+					let { stdout: i3_ZathuraCommOut } = await execAsync(`bash -c "comm -12 <(xdotool search --name  'org.pwmt.zathura'  | sort) <(xdotool search --name 'org.pwmt.zathura'  | sort)"`); 
 					let i3_ZathuraCommOutArray = i3_ZathuraCommOut.split("\n");
 					for (pid of i3_ZathuraCommOutArray) {
 						if (pid && pid.trim()) {
@@ -114,7 +114,7 @@ const server = net.createServer(async (socket) => {
 							catch (error) {}
 						}
 					}
-				}		catch (error) {notify(error)}
+				}		catch (error) {}
 				
 
 
