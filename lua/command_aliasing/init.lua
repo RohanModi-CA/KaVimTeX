7c2dfe474ff2c60caa4fdd67a3eec30cdcc3292f
Rohan Modi <61063602+RohanModi-CA@users.noreply.github.com>
2024-09-17 11:50:56 -0400

    check for \input -ed aliases

--- lua/command_aliasing/init.lua
+++ lua/command_aliasing/init.lua
@@ -18,60 +18,95 @@ end
 
 
 
+-- see if the file exists
+function file_exists(file)
+	local f = io.open(file, "rb")
+	if f then f:close() end
+	return f ~= nil
+end
+-- get all lines from a file, returns an empty list/table if the file does not exist
+function lines_from(file)
+	if not file_exists(file) then return {} end
+	local lines = {}
+	for line in io.lines(file) do 
+	lines[#lines + 1] = line
+	end
+	return lines
+end
+
+
+function search_handle_commands(line_array, file_to_write)
+	for i = 1, #line_array do
+		if stop_on_doc and string.find(line_array[i], "\\begin{document}") then
+			break
+		end
+
+		-- search to see if there are styles being \input-ed from other files, if so recurse and run this on them.
+		input_file = line_array[i]:match("\\input%s*{([^}]+)}")
+		local lines_of_inp = {}
+		if(input_file) then
+			lines_of_inp = lines_from(input_file)
+			search_handle_commands(lines_of_inp, file_to_write)
+		end
+
+		if string.find(line_array[i], "newcommand") then
+			if string.find(line_array[i], "\\newcommand") or string.find(line_array[i], "\\renewcommand") then
+				local term = string.sub(line_array[i], string.find(line_array[i], "{"), -1)
+
+				if string.find(line_array[i], "%%") then
+					term = string.sub(term, 1, string.find(line_array[i], "%%"))
+				end
+
+				local new_old = {}
+				local start_pos = 0
+				local brace_count = 0
+
+				for j = 1, #term do
+					local char = string.sub(term, j, j)
+					if char == '{' then
+						if brace_count == 0 then
+							start_pos = j + 1 
+						end
+						brace_count = brace_count + 1
+					elseif char == '}' then
+						brace_count = brace_count - 1
+						if brace_count == 0 then 
+							table.insert(new_old, string.sub(term, start_pos, j - 1))
+						end
+					end
+				end
+
+				-- Write pairs to the file
+				if #new_old >= 2 then -- Ensure we have at least two arguments
+					file_to_write:write(new_old[1] .. "KVTNEWCOMMAND" .. new_old[2] .. "\n")
+				end
+			end
+		end
+	end
+end
+
+
 
 
 -- Open "resources/aliases.txt" in write mode (overwrites existing content)
 local file = io.open(KVTRoot .. "/backend/resources/aliases.txt", "w")
+search_handle_commands(line_array, file)
 
-for i = 1, #line_array do
-    if stop_on_doc and string.find(line_array[i], "\\begin{document}") then
-        break
-    end
 
-    if string.find(line_array[i], "newcommand") then
-        if string.find(line_array[i], "\\newcommand") or string.find(line_array[i], "\\renewcommand") then
-            local term = string.sub(line_array[i], string.find(line_array[i], "{"), -1)
 
-            if string.find(line_array[i], "%%") then
-                term = string.sub(term, 1, string.find(line_array[i], "%%"))
-            end
 
-            local new_old = {}
-            local start_pos = 0
-            local brace_count = 0
 
-            for j = 1, #term do
-                local char = string.sub(term, j, j)
-                if char == '{' then
-                    if brace_count == 0 then
-                        start_pos = j + 1 
-                    end
-                    brace_count = brace_count + 1
-                elseif char == '}' then
-                    brace_count = brace_count - 1
-                    if brace_count == 0 then 
-                        table.insert(new_old, string.sub(term, start_pos, j - 1))
-                    end
-                end
-            end
 
-            -- Write pairs to the file
-            if #new_old >= 2 then -- Ensure we have at least two arguments
-                file:write(new_old[1] .. "KVTNEWCOMMAND" .. new_old[2] .. "\n")
-            end
-        end
-    end
-end
+
 
 -- add the universal fixes:
 local universal_fixes = [[
-\medskipKVTNEWCOMMAND.     .           
-\smallskipKVTNEWCOMMAND.   .
-\bigskipKVTNEWCOMMAND.        .                 
-\section*KVTNEWCOMMAND.
-\subsection*KVTNEWCOMMAND.
+	\medskipKVTNEWCOMMAND.     .           
+	\smallskipKVTNEWCOMMAND.   .
+	\bigskipKVTNEWCOMMAND.        .                 
+	\section*KVTNEWCOMMAND.
+	\subsection*KVTNEWCOMMAND.
 ]] -- in lua strings in [[]] do not require escape characters
-
 file:write(universal_fixes)
 
 
